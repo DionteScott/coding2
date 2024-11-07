3262722b07cc948b5569f07c683d05dfcfea5e62
DionteScott <dscott074@boyslatin.net>
2024-11-07 18:42:19 +0000

    pop quiz

---
+++ unit 3 javascript/jspopquiz.js
@@ -0,0 +1,41 @@
+// 1. How do you declare a variable in JavaScript? Please write your answer 
+// using JavaScript code. 
+//let x= 10
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript?
+// Please write your answer using JavaScript code. 
+console.log(a*b*c)
+
+// 3. Identify the following JavaScript operators and describe when you would use it?
+// Please write your answers using the string data type. 
+//  === equal value and equal type
+//  == equal to
+//  = x=y
+
+// 4. Describe a scenario where you would use a comparison operator,
+// and then provide a code example based on the scenerio you provided. 
+//In order to be able to determine the result between wo or more diffrent coding types and or compare
+// ===
+
+// 5. Describe a scenario where you would use a logical operator,
+// and then provide a code example based on the scenerio you provided. 
+//to find or figure out a proper variable and/or result of a greater source 
+//	(x < 10 && y > 1)
+
+// 6. Create three variables where when combined will result in the value of 36. 
+// Please use the console.log () to out put your result. 
+var a="10"
+var b="20"
+var c="8"
+console.log(a+b+c)
+// 7. Fill in the variable that will output the appropriate result. 
+
+var x ='.'
+var y = x *2
+
+// Your console log should output the value 28.
+console.log(28)
+
+// 8. What does it "concatenation" mean? Please write your response as a string
+// and also provide a code example of two variables being concatenated and output the result
+// using the console.log() function. 
