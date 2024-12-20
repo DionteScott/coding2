65cce5bf98c2294559373aed33b8c6b938e68909
DionteScott <dscott074@boyslatin.net>
2024-12-20 18:54:54 +0000

    quiz

---
+++ unit 3 javascript/Unit2Quiz.js
@@ -0,0 +1,57 @@
+//In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+"A group[ of code that does a specific action, processing it, and finalizing a result"
+//In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentenc as a string data typee.
+ "function parameter is a variable listed within a function definition acting as a placeholder  while a function argument is the actual value that is passed to the function when it is called"
+//In your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+"In Java, an if/else conditional statement is a function that allows you to control the flow of your program based on a specific condition."
+//What is an integer data type? Please write your response in a complete sentence as a string data type.
+"mumber"
+//What is a boolean data type? Please write your response in a complete sentence as a string data type.
+"representsd true and false"
+//Which operator would work best for the following function: An age verification function that needs to check if a user is a certain 
+// age and older to purchase a video game. Please write your answer as a string.
+"function argument"
+//Which operator would work best for the following function: An office security function that needs to have a specific alpha-numeric 
+// code to be entered in order to enter and exit a building. Please write your answer as a string.
+"if/else conditional statement"
+
+//You have been hired by Github to help them fix their password system. They would like you to create A function that will check how 
+// long a password is. Your program should be able to accept a alpha-numeric password and check if the password is more or less than 10 
+// characters long. if the password is longer than 10 characters, your program should tell the user that their password is too long. If the 
+// password is under 4 characters the function should tell the user the password is too short. If the password meets the length criteria,
+//  there should be a message confirming to the user that their password was created successfully.
+//HINT - you will need to use a function that counts the number of characters in a string. I recommend looking this up on W3schools.
+
+function passwordSystem(Password) {
+    let text = 12;
+    let length = 12;
+    if (password = )
+}
+
+//You have been hired as an engineer to develop a federal income tax function for a tax agency. They would like to be able to take in a 
+// user's annual income ammount and check how much money a person will need to pay in taxes based on the amount passed into the function.
+//Provided are income amounts and how much they will be taxed:
+
+//If a user makes between 0.00 and 12000.00 per year, they will be taxed 10%, plus an additional 3% for the state.
+
+//If a user makes between 12001.00 and 50000.00 per year, they will be taxed 12%, plus an additional 3% for the state.
+
+//If a user makes between 50001.00 and 103000.00, they will be taxed 22%, plus and additional 3% for the state.
+
+//If a user makes between 103001.00 and 198000.00, they will be taxed 23%, plus and additional 3% for the state.
+
+//Your function should return how much money the person owes in taxes based on the amount of money they provided.
+
+//You have applied to several colleges in the state of PA and as your final project, you must create a function that will inform you on whether you will be admitted into the school based on the schools' reccomendation letter criteria. Your function should take in the name of the school along with whether it is TRUE or FALSE that you recieved a reccomendation letter from one of the staff members at Boys Latin.
+//Provided are the schools you've applied to and their reccomendation letter criteria.
+
+//Temple Univesity - So long as you have 1 reccomendation letter from BL, you will get in.
+//Penn State - You must have 2 letters of recomendation from BL to get in.
+//University of Pennsylvania - You must have 2 letter from BL to get in.
+//Cheyney University - You must have 1 letter of reccomendation from BL to get in.
+//Provided are the letters of reccomendations you've recieved:
+
+//Both Mr. Parker and Mr. Kimble have given you a reccomendation letter to Cheyney University.
+//Mr. Kimble has given you a letter of reccomendation for Penn State, but Mr. Parker and Mr. Paris have not.
+//Mr. Paris has given you a letter of reccomendation to Temple University, but Mr. Parker and Mr. Kimble have not.
+//Mr. Parker has given you a letter of reccomnedation to The University of Pennsylvania, Mr. Kimble did not give you letter of reccomendation, But Mr. Paris did.
