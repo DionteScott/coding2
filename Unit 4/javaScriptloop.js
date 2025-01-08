7c8d3751c05f33aeb6449c6803356c2c8cfe70bf
DionteScott <dscott074@boyslatin.net>
2025-01-08 18:40:34 +0000

    Unit 4

---
+++ Unit 4/javaScriptloop.js
@@ -0,0 +1,19 @@
+
+function educationRank(gradeLevel) {
+    if (gradeLevel == 1) {
+        console.log("you are a freshmen in undergrad. ");
+    } else if (gradeLevel == 2) {
+    console.log("you are a sophmore in undergrad. ");
+} else if (gradeLevel == 3) {
+    console.log("you are a junior in undergrad.");
+} else if (gradeLevel == 4) {
+    console.log("you are a senior in undergrad.");
+    } else if (gradeLevel = 5 || gradeLevel ==6) {
+console.log("you are in a masters pregram in graduate school.")
+    } else {
+    console.log("Err. Please check you code.");
+}
+}
+
+educationRank(6);
+    
